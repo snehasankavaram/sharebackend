141a07591543ee52c27034369950e85cfca5853f
Sneha Sankavaram <snehasankavaram@berkeley.edu>
2015-12-05 13:43:04 -0800

    fixing bugs

--- Backend/share/app/controllers/users_controller.rb
+++ Backend/share/app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   protect_from_forgery
   def show
   	 @user = User.find_by(username: params[:username])
-     render json: { user: @user, profile: Profile.find(@user.user_profile_id}
+     render json: { user: @user, profile: Profile.find(@user.user_profile_id) }
   end
 
   def create
