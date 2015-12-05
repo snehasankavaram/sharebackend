e113bcc34be345d44ff8d514c4ac5ac63c0f4ea9
Sneha Sankavaram <snehasankavaram@berkeley.edu>
2015-12-05 15:28:41 -0800

    adding path to files controller

--- Backend/share/app/controllers/db_files_controller.rb
+++ Backend/share/app/controllers/db_files_controller.rb
@@ -5,7 +5,7 @@ class DbFilesController < ApplicationController
   	@user = User.find_by(username: params[:username])
   	drop_box_link = params[:link]
 
-  	new_file = DbFile.new(:link => drop_box_link)
+  	new_file = DbFile.new(file_params)
     new_file.view_count = 0
   	new_file.user = @user
   	new_file.save
@@ -16,13 +16,14 @@ class DbFilesController < ApplicationController
   	@file = DbFile.find(params[:id])
 
     #set view count
-  	@file.update(:view_count => params[:view_count])
+  	@file.update(file_params)
 
     #create new metadata object and set file of that object to this file
-    metadata = ViewMetadatum.new(:view_username => params[:viewed_by]);
-    metadata.db_file = @file
-    metadata.save
-
+    if params[:viewed_by] != nil
+      metadata = ViewMetadatum.new(:view_username => params[:viewed_by]);
+      metadata.db_file = @file
+      metadata.save
+    end
 
     head :ok, content_type: "text/html"
   end
@@ -46,4 +47,10 @@ class DbFilesController < ApplicationController
     @file.delete
     head :ok, content_type: "text/html"
   end
+
+  private
+    def file_params
+      params.permit(:link, :view_count, :local_path)
+    end
+
 end
